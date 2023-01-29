68bd56424466d0188b5b012968405105326028bb
Frank Pagan <frank@cocreate.app>
2023-01-29 20:55:16 +0000

    feat: import @cocreate/local-storage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+import action from '@cocreate/actions'
 
 const CoCreateLocalStorage = {
-	
+	support: true,
+    storage: new Map(),
+
 	init: function() {
 		var elements = document.querySelectorAll('[localstorage-set], [localstorage-get]');
 		this.initElements(elements)
         window.addEventListener('storage', function(e) {
             elements = document.querySelectorAll(`[localstorage-get="${e.key}"]`)
-            for (let element of elements)
-                this.get(element)
+            for (let element of elements) {
+                let value = this.getItem(key)
+                if (value != null){
+                    element.value = value;
+                }
+            }
 		});
 	},
 
@@ -21,79 +28,97 @@ const CoCreateLocalStorage = {
         element.addEventListener('input', (e) => {
             let isRealtime = element.getAttribute('realtime')
 			if (isRealtime == "false") return;
-			this.set(e.target)
 
             let key = e.target.getAttribute('localstorage-set');
-            let elements = document.querySelectorAll(`[localstorage-get="${e.key}"]`)
-            for (let el of elements){
-                if (el != element)
-                    this.get(el)
+            this.setItem(key, e.target.value);
+
+            let elements = document.querySelectorAll(`[localstorage-get="${key}"]`)
+            for (let el of elements) {
+                if (el != element) {
+                    let value = this.getItem(key)
+                    if (value != null){
+                        el.value = value;
+                    }
+                }
+        
             }
 		})
     },
     
-    get: function(element) {
-        let key = element.getAttribute('localstorage-get');
-        if (!key) return;
-        let value = window.localStorage.getItem(key)
-        if (value != null){
-            element.value = value;
-        }
+    getItem: function(key) {
+        if (this.support)
+            return window.localStorage.getItem(key)
+        else
+            return this.storage.get(key)
     }, 
     
-    set: function(element) {
-        let key = element.getAttribute('localstorage-set');
-        if (element.value && key){
-            window.localStorage.setItem(key, element.value);
-        }
+    setItem: function(key, value) {
+        if (this.support)
+            window.localStorage.setItem(key, value);
+        else
+            this.storage.set(key, value)
     },
-    
-    remove: function(element) {
-        element.value = '';
-        let key = element.getAttribute('localstorage-remove');
-        if (key) {
+
+    removeItem: function(key) {
+        if (this.support)
             window.localStorage.removeItem(key)
-        }
-    },
-    
+        else
+            this.storage.delete(key)
+    }, 
+        
     runStorage: function(btn) {
         const form = btn.form;
-        let storage = window.localStorage;
         if (!form) return;
         
         let set_els = form.querySelectorAll('[localstorage-set]')
-        set_els.forEach(input=>{
-            let key = input.getAttribute('localstorage-set');
-            if (input.value && key){
-                storage.setItem(key, input.value);
+        for (let el of set_els) {
+            let key = el.getAttribute('localstorage-set');
+            if (el.value && key){
+                this.setItem(key, el.value);
             }
-        });
+        }
         let get_els = form.querySelectorAll('[localstorage-get]')
-        get_els.forEach(input=>{
-            let key = input.getAttribute('localstorage-get');
+        for (let el of get_els) {
+            let key = el.getAttribute('localstorage-get');
             if (!key) return;
-            let value = storage.getItem(key)
+            let value = this.getItem(key)
             if (value != null){
-                input.value = value;
+                el.value = value;
             }
-        });
+        }
         let remove_els = form.querySelectorAll('[localstorage-remove]')
-        remove_els.forEach(input=>{
-            input.value = '';
-            let key = input.getAttribute('localstorage-remove');
+        for (let el of remove_els) {
+            el.value = '';
+            let key = el.getAttribute('localstorage-remove');
             if (key) {
-                storage.removeItem(key)
+                this.removeItem(key)
             }
-        });
+        }
         
         document.dispatchEvent(new CustomEvent('localStorage', {
 			detail: {}
 		}))
        
-    }
+    },
+
+    checkSupport: function() {
+        if (!('localStorage' in window)) {
+            console.log("This browser doesn't support localStorage.");
+            this.support = false;
+        } else { 
+            try {
+                let test = window.localStorage.getItem('test')
+                if (window.CoCreateConfig)
+                    if (window.CoCreateConfig.localStorage === false)
+                        this.support = false;
+            } catch(e) {
+                this.support = false;
+            }
+        }
+    }    
 }
 
-CoCreate.action.init({
+action.init({
 	name: "localStorage",
 	endEvent: "localStorage",
 	callback: (btn, data) => {
@@ -101,7 +126,7 @@ CoCreate.action.init({
 	},
 })
 
-CoCreate.action.init({
+action.init({
 	name: "localStorageRemove",
 	endEvent: "localStorageRemoved",
 	callback: (btn, data) => {
